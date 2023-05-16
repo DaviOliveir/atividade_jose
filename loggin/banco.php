5f5321846b32cdeef4c6ddfd8568fa1ed0cec0f7
Davi José batista Lima de Oliveira <112220168+DaviOliveir@users.noreply.github.com>
2023-05-15 22:00:49 -0300

    Add files via upload

---
+++ loggin/banco.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$servidor = 'localhost';
+$user = 'root';
+$password = 'root';
+$banco = 'bd_cadastro';
+
+$conexao = mysqli_connect($servidor,$user,$password,$banco);
+
+
+
+$nome = $_POST ['ome'];
+$email = $_POST ['mail'];
+$senha = $_POST ['enha'];
+$numerotel = $_POST ['umerotel'];
+
+
+
+
+
+
+$sql = "INSERT INTO tb_cadastro(id_cliente,nm_nome, nm_email, nr_senha, nr_telefone) 
+values (null,'$nome','$email','$senha','$numerotel')";
+$insert = mysqli_query($conexao, $sql);
+?>
---
+++ loggin/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+
+    <div>
+<img class="img" src="db8f35f34d13f55d811a00af38495b3d.jpg" alt="">
+
+    </div>
+
+    <form action="">
+
+        <label class="llogin" for="Login">Login</label>
+            <input class="ilogin" type="text" name="Login" id="Login">
+        <label class="lsenha" for="senha">senha</label>
+            <input class="isenha" type="password" name="senha" id="senha">
+            <input class="en" type="submit" value="Logar">
+            <a href="cadatro_cliente2.php" class="icas">Cadastrar</a>
+
+            
+
+
+    </form>
+
+    
+    
+</body>
+</html>
